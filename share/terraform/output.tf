b1de51f12c8447645efb50d26b478c163b84c454
Hugo Freire <hfreire@exec.sh>
2019-04-25 21:49:34 +0200

    fix: minor tweaks

--- share/terraform/output.tf
+++ share/terraform/output.tf
@@ -1,11 +1,17 @@
 output "aws_region" {
   value = "${var.aws_region}"
+
+  sensitive = true
 }
 
 output "aws_sns_topic_arn" {
   value = "${data.aws_sns_topic.selected.arn}"
+
+  sensitive = true
 }
 
 output "aws_cloudwatch_log_group_name" {
   value = "${data.aws_cloudwatch_log_group.selected.name}"
+
+  sensitive = true
 }
