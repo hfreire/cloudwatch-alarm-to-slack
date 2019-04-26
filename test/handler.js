5b025223a33b7451948d0a394d068ab9047413d6
Hugo Freire <hfreire@exec.sh>
2019-04-26 21:33:00 +0200

    chore(copyright): update copyright license

--- test/handler.js
+++ test/handler.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Hugo Freire <hugo@antifragile.systems>.
+ * Copyright (c) 2019, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
